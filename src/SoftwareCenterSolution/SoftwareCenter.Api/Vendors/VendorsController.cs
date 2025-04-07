adcd0cd76e5cfcfba27520c6ca9401618f72ff50
Johnny Guzman <guzmanj774@gmail.com>
2025-04-07 21:33:36 +0000

    web api 100, day 1

---
+++ src/SoftwareCenterSolution/SoftwareCenter.Api/Vendors/VendorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareCenter.Api.Vendors;
+
+public class VendorsController : ControllerBase
+{
+    [HttpPost("/vendors")]
+    public async Task<ActionResult> AddAVendorAsync()
+    {
+        return Ok();
+    }
+}
---
+++ src/SoftwareCenterSolution/SoftwareCenter.Tests/Vendors/AddingAVendor.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Net.Http.Json;
+using Alba;
+
+namespace SoftwareCenter.Tests.Vendors;
+
+public class AddingAVendor
+{
+
+    [Fact]
+    public async Task CanAddVendorAsync()
+    {
+
+        var host = await AlbaHost.For<Program>();
+        // about 15 lines from that documentation, start up the api with our Program.cs configuration.
+
+
+        // System Tests are "scenarios".  
+        await host.Scenario(api =>
+        {
+            api.Post.Json(new { }).ToUrl("/vendors");
+            api.StatusCodeShouldBeOk();
+        });
+
+        //  // a "black box" test and can be SUPER flaky in your pipelines.
+
+        //// bad. don't do this. don't even try to make this code better.
+        //  var client = new HttpClient();
+        //  client.BaseAddress = new Uri("http://localhost:1337");
+
+        //  var response = await client.PostAsJsonAsync("vendors", new { });
+
+        //  Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+    }
+}
